41da32d8a410d124dea16f17f8759e7f9246d39e
Михаил Петраков <xrynja23@gmail.com>
2024-12-29 20:15:14 +0700

    Арифметика 2

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, make_response, redirect
+from flask import Blueprint, render_template, request, redirect
 lab4 = Blueprint('lab4', __name__)
 
 @lab4.route('/lab4/')
@@ -21,3 +21,63 @@ def div():
     x2 = int(x2)
     result = x1/x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+@lab4.route('/lab4/sum', methods = ['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = '0'
+    if x2 == '':
+        x2 = '0'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+@lab4.route('/lab4/umn-form')
+def umn_form():
+    return render_template('lab4/umn-form.html')
+@lab4.route('/lab4/umn', methods = ['POST'])
+def umn():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = '1'
+    if x2 == '':
+        x2 = '1'
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 * x2
+    return render_template('lab4/umn.html', x1=x1, x2=x2, result=result)
+@lab4.route('/lab4/vch-form')
+def vch_form():
+    return render_template('lab4/vch-form.html')
+@lab4.route('/lab4/vch', methods = ['POST'])
+def vch():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/vch.html', error='Оба поля должны быть заполнены!')
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/vch.html', x1=x1, x2=x2, result=result)
+@lab4.route('/lab4/vozv-form')
+def vozv_form():
+    return render_template('lab4/vozv-form.html')
+@lab4.route('/lab4/vozv', methods = ['POST'])
+def vozv():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/vozv.html', error='Оба поля должны быть заполнены!')
+    if x1 == '0' and x2 == '0':
+        return render_template('lab4/vozv.html', error='не должно быть 0!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 ** x2
+    return render_template('lab4/vozv.html', x1=x1, x2=x2, result=result)
