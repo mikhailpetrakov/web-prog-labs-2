50e45450b8cb881999d290f19ee50d4d8c1ff105
Петраков Михаил <xrynja23@gmail.com>
2025-02-13 04:57:07 +0700

    Доп задание

--- lab9.py
+++ lab9.py
@@ -1,42 +1,48 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, session
 lab9 = Blueprint('lab9', __name__)
 
 
 @lab9.route('/lab9/')
 def main():
+    if 'name' in session and 'age' in session and 'gender' in session and 'preference1' in session :
+        return redirect(url_for('lab9.step5'))
     return render_template('lab9/index.html')
 
 @lab9.route('/lab9/step1', methods=['POST'])
 def step1():
-    name = request.form.get('name')
-    return render_template('lab9/step2.html', name=name)
+    session['name'] = request.form.get('name')
+    return render_template('lab9/step2.html')
 
 @lab9.route('/lab9/step2', methods=['POST'])
 def step2():
-    name = request.form.get('name')
-    age = request.form.get('age')
-    return render_template('lab9/step3.html', name=name, age=age)
+    session['age'] = request.form.get('age')
+    return render_template('lab9/step3.html')
 
 @lab9.route('/lab9/step3', methods=['POST'])
 def step3():
-    name = request.form.get('name')
-    age = request.form.get('age')
-    gender = request.form.get('gender')
-    return render_template('lab9/step4.html', name=name, age=age, gender=gender)
+    session['gender'] = request.form.get('gender')
+    return render_template('lab9/step4.html')
 
 @lab9.route('/lab9/step4', methods=['POST'])
 def step4():
-    name = request.form.get('name')
-    age = request.form.get('age')
-    gender = request.form.get('gender')
-    preference1 = request.form.get('preference1')
-    return render_template('lab9/step5.html', name=name, age=age, gender=gender, preference1=preference1)
+    session['preference1'] = request.form.get('preference1')
+    return render_template('lab9/step5.html')
 
-@lab9.route('/lab9/step5', methods=['POST'])
+@lab9.route('/lab9/step5', methods=['POST','GET'])
 def step5():
-    name = request.form.get('name')
-    age = request.form.get('age')
-    gender = request.form.get('gender')
-    preference1 = request.form.get('preference1')
-    preference2 = request.form.get('preference2')
+    if session.get('preference2') == None:
+        session['preference2'] = request.form.get('preference2')
+        
+    preference2 = session.get('preference2')
+    name = session.get('name')
+    age = session.get('age')
+    gender = session.get('gender')
+    preference1 = session.get('preference1')
+    
     return render_template('lab9/final.html', name=name, age=age, gender=gender, preference1=preference1, preference2=preference2)
+
+@lab9.route('/lab9/reset')
+def reset():
+    # Очищаем сессию и перенаправляем на главную страницу
+    session.clear()
+    return redirect(url_for('lab9.main'))
